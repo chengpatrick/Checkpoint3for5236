d08da686fd8a16b15e67866105dcbdc51cefa5c2
chengpatrick <qq240766532@gmail.com>
2021-10-02 20:50:58 -0400

    uploaded image for sign up

--- app/src/main/java/com/example/checkpoint3for5236/MainActivity.java
+++ app/src/main/java/com/example/checkpoint3for5236/MainActivity.java
@@ -3,19 +3,41 @@ package com.example.checkpoint3for5236;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.checkpoint3for5236.databinding.ActivityMainBinding;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        TextView username=(TextView) findViewById(R.id.editTextTextPersonName2);
+        TextView password=(TextView) findViewById(R.id.editTextTextPassword2);
+
+        Button loginbtn=(Button)  findViewById(R.id.button);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin")&& password.getText().toString().equals("admin")){
+                    //correct password
+                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //incorrect password
+                    Toast.makeText(MainActivity.this,"Login Fail, please check username or password",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
 
     }
