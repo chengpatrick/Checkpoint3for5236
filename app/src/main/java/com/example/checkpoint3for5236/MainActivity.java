4041994f446bcdb5a537577a72f042df53c57cb5
chengpatrick <qq240766532@gmail.com>
2021-10-03 00:12:28 -0400

    created log for lifecycle, added finish() for SignUpActivity

--- app/src/main/java/com/example/checkpoint3for5236/MainActivity.java
+++ app/src/main/java/com/example/checkpoint3for5236/MainActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final  String TAG="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG,"onCreate");
 
         TextView username=(TextView) findViewById(R.id.editTextTextPersonName3);
         TextView password=(TextView) findViewById(R.id.editTextTextPassword3);
@@ -44,4 +47,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy");
+    }
+
 }
--- app/src/main/java/com/example/checkpoint3for5236/SignUpActivity.java
+++ app/src/main/java/com/example/checkpoint3for5236/SignUpActivity.java
@@ -2,10 +2,13 @@ package com.example.checkpoint3for5236;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
@@ -14,6 +17,7 @@ import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
     private static final String file_name="accounts_example.txt";
+    private static final  String TAG="SignUpActivity";
 
     EditText uEditText;
     EditText pEditText;
@@ -21,16 +25,33 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i(TAG,"onCreate");
+
         setContentView(R.layout.activity_sign_up);
 
         uEditText=findViewById(R.id.editTextTextPersonName3);
         pEditText=findViewById(R.id.editTextTextPassword3);
 
         Button signupbtn=(Button)  findViewById(R.id.signinButton);
+        TextView signUp = (TextView) findViewById(R.id.textView3);
 
+        //already have an account, go back to main login
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+
+                //destroy
+                finish();
+            }
+        });
+
+        //enter info and sign up new account
         signupbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if(uEditText.getText().toString().length()>0 && pEditText.getText().toString().length()>0){
                     //correct password
                     save(view);
@@ -40,6 +61,9 @@ public class SignUpActivity extends AppCompatActivity {
                     //incorrect password
                     Toast.makeText(SignUpActivity.this,"Please Enter username and password to register",Toast.LENGTH_SHORT).show();
                 }
+
+                //destroy
+                finish();
             }
         });
 
@@ -74,6 +98,41 @@ public class SignUpActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy");
     }
 
 }
