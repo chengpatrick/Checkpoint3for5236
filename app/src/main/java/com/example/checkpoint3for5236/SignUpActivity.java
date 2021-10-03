a550f8fa3f457f74c443c247c73380a6598d9b53
Xiaoliang020 <chen.8182@osu.edu>
2021-10-02 21:43:20 -0400

    Add Sign Up activity

--- app/src/main/java/com/example/checkpoint3for5236/SignUpActivity.java
+++ app/src/main/java/com/example/checkpoint3for5236/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.checkpoint3for5236;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        TextView signUp = (TextView) findViewById(R.id.textView3);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+            }
+        });
     }
 }
