d2cd4e27e1fd871ff4b3131b8ab16cea3d177a45
chengpatrick <qq240766532@gmail.com>
2021-10-02 20:18:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/checkpoint3for5236/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.checkpoint3for5236;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.checkpoint3for5236.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+
+
+    }
+}
